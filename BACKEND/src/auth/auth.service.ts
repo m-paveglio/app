b6bff5b092859b6392ebb158ddb0fa8b64188678
m-paveglio <110037143+m-paveglio@users.noreply.github.com>
2023-11-22 22:18:01 -0300

    hashing - ok

--- BACKEND/src/auth/auth.service.ts
+++ BACKEND/src/auth/auth.service.ts
@@ -1,23 +1,34 @@
-import { Injectable, Inject, UnauthorizedException} from '@nestjs/common';
+import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { user } from 'BACKEND/src/user/user.entity';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(@Inject('USER_REPOSITORY')
-  private userRepository: Repository<user>,
-  private Jwtservice: JwtService) {}
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<user>,
+    private Jwtservice: JwtService,
+  ) {}
 
   async signIn(cpf: string, password: string): Promise<any> {
-    const user = await this.userRepository.findOne({where:{cpf}});
-    if (user?.password !== password) {
+    const user = await this.userRepository.findOne({ where: { cpf } });
+
+    if (!user) {
       throw new UnauthorizedException();
     }
-    const payload = {sub: user.cpf};
-    return {
-      access_token: await this.Jwtservice.signAsync(payload)
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
     }
+
+    const payload = { sub: user.cpf };
+
+    return {
+      access_token: await this.Jwtservice.signAsync(payload),
+    };
   }
 }
