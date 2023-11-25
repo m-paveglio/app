09922867c9aa5f5dd9dc8bcb9adaffaa1a5094cc
m-paveglio <110037143+m-paveglio@users.noreply.github.com>
2023-11-25 19:49:24 -0300

    ok

--- FRONTEND/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserComponent } from './components/user/user.component';
-
-export const routes: Routes = [
-  {
-  path: 'user',
-  component: UserComponent
-}
-];
