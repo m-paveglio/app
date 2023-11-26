93816ea459b5bc2755e52ccd183b87dbecf3b0dd
m-paveglio <110037143+m-paveglio@users.noreply.github.com>
2023-11-26 15:22:24 -0300

    OK

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+export const routes: Routes = [
+{
+  path: 'login',
+  component: LoginComponent
+}
+];
+  
---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, LoginComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
