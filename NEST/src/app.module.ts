8ffd07f04229ce52ba6aadafaa108db1421d4834
Marlon Guimarães Paveglio <marlon.paveglio@NOTACONTROLSM.LOCAL>
2024-10-29 15:21:09 -0300

    teste

--- NEST/src/app.module.ts
+++ NEST/src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { userModule } from './Login/user/user.module';
 import { AuthModule } from './Login/auth/auth.module';
-import { PermissoesModule } from './Geral/Outros/Permissoes/permissoes.module';
-import { UfModule } from './Geral/Outros/Endereco/UF/uf.module';
-import { PaisModule } from './Geral/Outros/Endereco/Pais/pais.module';
-import { CidadesModule } from './Geral/Outros/Endereco/Cidades/cidades.module';
-import { LogradouroModule } from './Geral/Outros/Endereco/Logradouro/logradouro.module';
-import { ProfissoesModule } from './Geral/Outros/Profissoes/profissoes.module';
+import { PermissoesModule } from './Geral/Permissoes/permissoes.module';
+import { UfModule } from './Geral/Endereco/UF/uf.module';
+import { PaisModule } from './Geral/Endereco/Pais/pais.module';
+import { CidadesModule } from './Geral/Endereco/Cidades/cidades.module';
+import { LogradouroModule } from './Geral/Endereco/Logradouro/logradouro.module';
+import { ProfissoesModule } from './Geral/Profissoes/profissoes.module';
 import { pessoasModule } from './Geral/Pessoas/pessoas.module';
 
 
