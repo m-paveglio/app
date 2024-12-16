2bb6f76d028e7acc820d09766ac9241b8e97f83b
m-paveglio <marlonpaveglio91@outlook.com>
2024-12-16 16:01:34 -0300

    ajuste mensagem de erro no login

--- ANGULAR/src/app/Geral/login/login.component.ts
+++ ANGULAR/src/app/Geral/login/login.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';  // Importe o MessageService
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [MessageService]  // Adicione o MessageService como provider
 })
 export class LoginComponent {
   credentials = { CPF: '', SENHA: '' };
-  mensagem: string | null = null;
-  isLoginSucesso: boolean = false;
-  exibirMensagem: boolean = false;
 
-  constructor(private loginService: LoginService, private router: Router) { }
+  constructor(private loginService: LoginService, private router: Router, private messageService: MessageService) { }
 
   onCPFInputChange(event: any) {
-    // Atualiza o CPF removendo pontos e hífens enquanto o usuário digita
     this.credentials.CPF = event.target.value.replace(/[.\-]/g, '');
   }
 
@@ -25,13 +23,13 @@ export class LoginComponent {
   }
 
   onLogin() {
-    // Remove os pontos e hífens do CPF antes de fazer login
     this.credentials.CPF = this.credentials.CPF.replace(/[.\-]/g, '');
 
     this.loginService.login(this.credentials).subscribe(
       (response) => {
         console.log('Login bem-sucedido!', response);
         this.router.navigate(['/dashboard']);
+        this.mostrarMensagem('Login bem-sucedido!', true);
       },
       (error) => {
         console.error('Erro no login:', error);
@@ -42,18 +40,18 @@ export class LoginComponent {
 
   processarErro(mensagemErro: string) {
     if (mensagemErro.includes('Credenciais inválidas')) {
-      this.mostrarMensagem('Credenciais inválidas, verifique seu CPF ou Senha.', false);
+      this.mostrarMensagem('Verifique seu CPF ou Senha.', false);
     } else {
       this.mostrarMensagem('Erro ao tentar fazer login. Tente novamente mais tarde.', false);
     }
   }
 
   mostrarMensagem(mensagem: string, sucesso: boolean) {
-    this.mensagem = mensagem;
-    this.isLoginSucesso = sucesso;
-    this.exibirMensagem = true;
-    setTimeout(() => {
-      this.exibirMensagem = false;
-    }, 3000);
+    this.messageService.add({
+      severity: sucesso ? 'success' : 'error',
+      summary: sucesso ? 'Sucesso' : 'Credenciais inválidas',
+      detail: mensagem,
+      life: 3000  // Tempo para o toast desaparecer (em milissegundos)
+    });
   }
 }
