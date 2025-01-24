62178dd804a0ef2f28761bcea6e0bc69d0476a07
m-paveglio <marlonpaveglio91@outlook.com>
2025-01-24 15:27:13 -0300

    implementação emissão NFS-e, ajustando assinatura do XML

--- NEST/src/Geral/Nfse/nfse.service.ts
+++ NEST/src/Geral/Nfse/nfse.service.ts
@@ -1,9 +1,12 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import * as fs from 'fs';
 import * as https from 'https';
+import * as forge from 'node-forge';
 import { XmlUtilsService } from './common/xml-utils.service';
 import { EmailService } from './common/email.service';
+import { parseStringPromise, Builder } from 'xml2js';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class NfseService {
@@ -16,22 +19,43 @@ export class NfseService {
   ) {}
 
   async enviarLoteRps(dados: any): Promise<any> {
-    // Gerar XML
+    // Gerar XML base
     const xml = await this.xmlUtils.gerarXml('enviar-lote-rps', dados);
-  
     console.log('XML Gerado:', xml);
-  
+
+    // Carregar certificado e chave privada do PFX
+    const pfx = fs.readFileSync('certificado.pfx');
+    const passphrase = 'minha_senha';
+    const p12Asn1 = forge.asn1.fromDer(pfx.toString('binary'));
+    const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, passphrase);
+
+    const keyBag = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag });
+    const certBag = p12.getBags({ bagType: forge.pki.oids.certBag });
+
+    if (!keyBag || Object.keys(keyBag).length === 0) {
+      throw new Error('Chave privada não encontrada no certificado PFX.');
+    }
+    if (!certBag || Object.keys(certBag).length === 0) {
+      throw new Error('Certificado público não encontrado no certificado PFX.');
+    }
+
+    const privateKey = forge.pki.privateKeyToPem(keyBag[Object.keys(keyBag)[0]][0].key);
+    const publicCert = forge.pki.certificateToPem(certBag[Object.keys(certBag)[0]][0].cert);
+
+    // Assinar o XML
+    const xmlAssinado = await this.assinarXml(xml, privateKey, publicCert);
+    console.log('XML Assinado:', xmlAssinado);
+
     // Configurar HTTPS com certificado
     const httpsAgent = new https.Agent({
-      pfx: fs.readFileSync('certificado.pfx'),
-      passphrase: 'minha_senha',
+      pfx,
+      passphrase,
       minVersion: 'TLSv1.2',
     });
-  
+
     try {
-      // Enviar requisição SOAP
       const response = await this.httpService
-        .post(this.nfseEndpoint, xml, {
+        .post(this.nfseEndpoint, xmlAssinado, {
           headers: {
             'Content-Type': 'text/xml;charset=utf-8',
             'SOAPAction': 'http://nfse.abrasf.org.br/RecepcionarLoteRps',
@@ -39,15 +63,147 @@ export class NfseService {
           httpsAgent,
         })
         .toPromise();
-  
+
       console.log('Resposta do Servidor:', response.data);
       return response.data;
-  
     } catch (error) {
       console.error('Erro ao enviar a requisição:', error.response?.data || error.message);
       throw error;
     }
   }
+
+  private async assinarXml(xml: string, privateKey: string, publicCert: string): Promise<string> {
+    const xmlParsed = await parseStringPromise(xml);
+
+    // Processar cada <Rps>
+    const listaRps = xmlParsed['soap:Envelope']['soap:Body'][0]['svc:RecepcionarLoteRps'][0]['nfseDadosMsg'][0]['EnviarLoteRpsEnvio'][0]['LoteRps'][0]['ListaRps'][0]['Rps'];
+
+    listaRps.forEach((rps: any) => {
+        const infDeclaracaoTag = rps['InfDeclaracaoPrestacaoServico'][0];
+
+        // Verificar se já existe uma tag <Signature> neste RPS
+        if (rps['Signature']) {
+            console.warn('A tag <Signature> já existe neste RPS. Ignorando adição.');
+            return;
+        }
+
+        // Canonicalizar <InfDeclaracaoPrestacaoServico>
+        const canonicalXml = this.canonicalizeXml(infDeclaracaoTag);
+
+        // Calcular DigestValue
+        const digest = crypto.createHash('sha1').update(canonicalXml).digest('base64');
+
+        // Assinar o DigestValue
+        const sign = crypto.createSign('RSA-SHA1');
+        sign.update(canonicalXml);
+        const signatureValue = sign.sign(privateKey, 'base64');
+
+        // Certificado em Base64
+        const x509Certificate = publicCert.replace(/(-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----|\n|\r)/g, '');
+
+        // Construir a estrutura da assinatura
+        rps['Signature'] = [{
+            $: { xmlns: 'http://www.w3.org/2000/09/xmldsig#' },
+            SignedInfo: [{
+                CanonicalizationMethod: [{
+                    $: { Algorithm: 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315' },
+                }],
+                SignatureMethod: [{
+                    $: { Algorithm: 'http://www.w3.org/2000/09/xmldsig#rsa-sha1' },
+                }],
+                Reference: [{
+                    Transforms: [{
+                        Transform: [
+                            { $: { Algorithm: 'http://www.w3.org/2000/09/xmldsig#enveloped-signature' } },
+                            { $: { Algorithm: 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315' } },
+                        ],
+                    }],
+                    DigestMethod: [{ $: { Algorithm: 'http://www.w3.org/2000/09/xmldsig#sha1' } }],
+                    DigestValue: [digest],
+                }],
+            }],
+            SignatureValue: [signatureValue],
+            KeyInfo: [{
+                X509Data: [{
+                    X509Certificate: [x509Certificate],
+                }],
+            }],
+        }];
+    });
+
+    // Gerar assinatura global para o <LoteRps>
+    const loteRpsTag = xmlParsed['soap:Envelope']['soap:Body'][0]['svc:RecepcionarLoteRps'][0]['nfseDadosMsg'][0]['EnviarLoteRpsEnvio'][0]['LoteRps'][0];
+
+    let loteAssinaturaXml = '';
+    if (!loteRpsTag['Signature']) {
+        const canonicalLoteXml = this.canonicalizeXml(loteRpsTag);
+
+        const loteDigest = crypto.createHash('sha1').update(canonicalLoteXml).digest('base64');
+        const loteSign = crypto.createSign('RSA-SHA1');
+        loteSign.update(canonicalLoteXml);
+        const loteSignatureValue = loteSign.sign(privateKey, 'base64');
+
+        const x509Certificate = publicCert.replace(/(-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----|\n|\r)/g, '');
+
+        // Construir a estrutura da assinatura global como string
+        loteAssinaturaXml = `
+          <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
+            <SignedInfo>
+              <CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315" />
+              <SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1" />
+              <Reference>
+                <Transforms>
+                  <Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature" />
+                  <Transform Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315" />
+                </Transforms>
+                <DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1" />
+                <DigestValue>${loteDigest}</DigestValue>
+              </Reference>
+            </SignedInfo>
+            <SignatureValue>${loteSignatureValue}</SignatureValue>
+            <KeyInfo>
+              <X509Data>
+                <X509Certificate>${x509Certificate}</X509Certificate>
+              </X509Data>
+            </KeyInfo>
+          </Signature>
+        `.trim();
+    } else {
+        console.warn('Tag <Signature> já existe no <LoteRps>. Ignorando adição.');
+    }
+
+    // Reconstruir o XML
+    const builder = new Builder({ headless: true, renderOpts: { pretty: false } });
+    let xmlAssinado = builder.buildObject(xmlParsed);
+
+    // Adicionar a assinatura global após </LoteRps>
+    if (loteAssinaturaXml) {
+        xmlAssinado = xmlAssinado.replace(
+            '</LoteRps>',
+            `</LoteRps>${loteAssinaturaXml}`
+        );
+    }
+
+    return xmlAssinado
+      .replace(/&#xD;/g, '') // Remover quebras de linha desnecessárias
+      .replace(/&(lt|gt|amp);/g, match => {
+        if (match === '&lt;') return '<';
+        if (match === '&gt;') return '>';
+        if (match === '&amp;') return '&';
+        return match;
+      });
+}
+
+
+
+private canonicalizeXml(xmlObject: any): string {
+    const builder = new Builder({ headless: true, renderOpts: { pretty: false } });
+    return builder.buildObject(xmlObject)
+        .replace(/\r?\n|\r/g, '') // Remover quebras de linha
+        .trim(); // Remover espaços em branco desnecessários
+}
+
+
 
   async consultarSituacao(dados: any): Promise<any> {
     const xml = await this.xmlUtils.gerarXml('consultar-situacao', dados);
