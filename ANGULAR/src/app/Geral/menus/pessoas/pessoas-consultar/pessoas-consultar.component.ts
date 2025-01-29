891b58eeb17d303ab59c47b56b74a14f5476c392
m-paveglio <marlonpaveglio91@outlook.com>
2025-01-29 09:24:18 -0300

    ajustando pessoas consultar e incluir, deve ser incluído a validação de CPF e CNPJ para criação

--- ANGULAR/src/app/Geral/menus/pessoas/pessoas-consultar/pessoas-consultar.component.ts
+++ ANGULAR/src/app/Geral/menus/pessoas/pessoas-consultar/pessoas-consultar.component.ts
@@ -5,9 +5,20 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 // Interface para tipar os dados da pessoa
 interface Pessoa {
   CPF: string;
+  CPF_CNPJ?: string; // Adicionado
   NOME: string;
   EMAIL?: string;
   TIPO_USER?: string;
+  DDD?: string; // Adicionado
+  TELEFONE_CELULAR?: string; // Adicionado
+  CEP?: string;
+  RUA_LOGRADOURO?: string;
+  NUMERO_LOGRADOURO?: string;
+  BAIRRO_LOGRADOURO?: string;
+  COMPLEMENTO_LOGRADOURO?: string;
+  CIDADE?: string;
+  COD_IBGE?: string;
+  UF?: string;
 }
 
 @Component({
@@ -17,11 +28,12 @@ interface Pessoa {
   providers: [MessageService, ConfirmationService],
 })
 export class PessoasConsultarComponent {
-  CPF: string = '';
+  CPF_CNPJ: string = '';
   nome: string = '';
   resultado: Pessoa | null = null; // Tipado como Pessoa
   pessoasEncontrados: Pessoa[] = []; // Lista de pessoas encontradas
   editMode = false;
+  novaPessoa: any = {};
 
   constructor(
     private pessoasService: PessoasService,
@@ -30,23 +42,23 @@ export class PessoasConsultarComponent {
   ) {}
 
   // Validação de CPF
-  isCpfValido(CPF: string): boolean {
-    if (!CPF || CPF.length !== 11) return false;
+  isCpfValido(CPF_CNPJ: string): boolean {
+    if (!CPF_CNPJ || CPF_CNPJ.length !== 11) return false;
     let soma = 0,
       resto;
-    if (CPF === '00000000000') return false;
-    for (let i = 1; i <= 9; i++) soma += parseInt(CPF.substring(i - 1, i)) * (11 - i);
+    if (CPF_CNPJ === '00000000000') return false;
+    for (let i = 1; i <= 9; i++) soma += parseInt(CPF_CNPJ.substring(i - 1, i)) * (11 - i);
     resto = (soma * 10) % 11;
     if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(CPF.substring(9, 10))) return false;
+    if (resto !== parseInt(CPF_CNPJ.substring(9, 10))) return false;
     soma = 0;
-    for (let i = 1; i <= 10; i++) soma += parseInt(CPF.substring(i - 1, i)) * (12 - i);
+    for (let i = 1; i <= 10; i++) soma += parseInt(CPF_CNPJ.substring(i - 1, i)) * (12 - i);
     resto = (soma * 10) % 11;
-    return resto === parseInt(CPF.substring(10, 11));
+    return resto === parseInt(CPF_CNPJ.substring(10, 11));
   }
 
   buscarpessoas() {
-    if (this.CPF) {
+    if (this.CPF_CNPJ) {
       // Busca por CPF
       this.buscarPorCpf();
     } else if (this.nome) {
@@ -58,7 +70,7 @@ export class PessoasConsultarComponent {
   }
   
   buscarPorCpf() {
-    this.pessoasService.buscarPorCpf(this.CPF).subscribe(
+    this.pessoasService.buscarPorCpf(this.CPF_CNPJ).subscribe(
       (data) => {
         if (data) {
           this.resultado = data; // Exibe os detalhes da pessoa
@@ -133,6 +145,30 @@ export class PessoasConsultarComponent {
       (error) => {
         console.error('Erro ao excluir pessoa:', error);
         this.showError('Erro ao excluir a pessoa.');
+      }
+    );
+  }
+
+  buscarEnderecoPorCEP(cep: string) {
+
+    this.pessoasService.getEnderecoPorCEP(cep).subscribe(
+      (endereco) => {
+        // Preenche os campos do formulário com os dados do endereço
+        this.novaPessoa.RUA_LOGRADOURO = endereco.logradouro;
+        this.novaPessoa.BAIRRO_LOGRADOURO = endereco.bairro;
+        this.novaPessoa.CIDADE = endereco.cidade;
+        this.novaPessoa.UF = endereco.uf;
+        this.novaPessoa.COD_IBGE = endereco.cod_ibge;
+      },
+      (error) => {
+        console.error('Erro ao buscar endereço:', error);
+        // Limpa os campos de endereço
+        this.novaPessoa.RUA_LOGRADOURO = '';
+        this.novaPessoa.BAIRRO_LOGRADOURO = '';
+        this.novaPessoa.CIDADE = '';
+        this.novaPessoa.UF = '';
+        this.novaPessoa.COD_IBGE = '';
+        this.showError('Erro ao buscar endereço. Verifique o CEP informado.');
       }
     );
   }
